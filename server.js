64b34cb48685129cec878c531c1650d1546c18aa
geoffreyyip <geoffreyyip2@gmail.com>
2017-05-30 18:37:26 -0400

    Render nextBusArrivals into template
    This commit injects the data from nextBusArrival() API call into a HTML
    template for server-side rendering. This ensures the client will receive
    a fully-populated page on first load.
    
    This commit uses a hardcoded API call to verify template engine works.
    The hardcoded API call will be removed in the future.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 
+const nextBusArrivals = require('./api/nextBusArrivals');
+
 const app = express();
 
 app.set('port', process.env.PORT || 4444);
@@ -15,7 +17,11 @@ app.set('view engine', 'nunjucks');
 
 // routes
 app.get('/', (req, res) => {
-  res.render('index.html');
+  // hard-coded for sanity testing
+  nextBusArrivals('MTA NYCT_B63', 308209)
+    .then((arrivals) => {
+      res.render('index.html', {arrivals});
+    });
 });
 
 app.listen(app.get('port'), () => {
