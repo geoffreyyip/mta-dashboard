d05b95678217c3aa5056c0968c49a5a159199062
geoffreyyip <geoffreyyip2@gmail.com>
2017-06-02 17:46:45 -0400

    Create DateRange class
    The MTA divides its Planned Work into workday batches (Mon - Fri) and
    weekend batches (Saturday and Sunday). This DateRange class will
    assist the crawler script in generating URLS for different workday /
    weekend batches.

---
+++ getPlannedWork/DateRange.js
@@ -0,0 +1,56 @@
+var moment = require('moment');
+
+var MONDAY = 1;
+var FRIDAY = 5;
+var SATURDAY = 6;
+var SUNDAY = 7;
+
+class DateRange {
+  constructor(date) {
+    const datetime = moment(date);
+    this.type = this.getType(datetime);
+
+    if (this.type === 'workday') {
+      this.start = moment(datetime).isoWeekday(MONDAY);
+      this.end = moment(datetime).isoWeekday(FRIDAY);
+    } else {
+      this.start = datetime.clone().isoWeekday(SATURDAY);
+      this.end = datetime.clone().isoWeekday(SUNDAY);
+    };
+  }
+
+  next(offset=1) {
+    const changeType = offset % 2 === 1;
+    const weeksToAdd = Math.floor(offset / 2);
+
+    const nextStart = this.start.clone();
+
+    if (changeType) {
+      if (this.type === 'workday')
+        nextStart.isoWeekday(SATURDAY);
+      else if (this.type === 'weekend') {
+        nextStart.add(1, 'week').isoWeekday(MONDAY);
+      }
+    }
+
+    nextStart.add(weeksToAdd, 'week');
+
+    return new DateRange(nextStart);
+  }
+
+  getType(date) {
+    if (this.isWorkday(date)) return 'workday';
+    else return 'weekend';
+  }
+
+  isWorkday(date) {
+    return date.isoWeekday() < SATURDAY;
+  }
+
+  isWeekend(date) {
+    return date.isoWeekday() >= SATURDAY;
+  }
+}
+
+
+module.exports = DateRange;
