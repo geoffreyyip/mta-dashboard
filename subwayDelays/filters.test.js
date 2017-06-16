bcb16f38c83d2916272b27d183dd2aa60d9493ad
geoffreyyip <geoffreyyip2@gmail.com>
2017-06-15 23:39:27 -0400

    Implement advisory filters for routes and deduping
    These filters will get invoked by a future cleanser script.

---
+++ subwayDelays/filters.test.js
@@ -0,0 +1,59 @@
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+
+chai.use(chaiAsPromised);
+chai.should();
+
+const {
+  removeDuplicates,
+  makeRoutesFilter,
+  flattenToArray,
+} = require('./filters.js');
+
+const workBatches = require('./example.json');
+
+
+describe('removeDuplicates', function() {
+
+  let advisories;
+
+  beforeEach(function() {
+    advisories = workBatches[0].advisories;
+  });
+
+  it('should return an array of distinct values', function() {
+    const unique = removeDuplicates(advisories);
+    const flat = flattenToArray(unique);
+    flat.length.should.equal(new Set(flat).size);
+  });
+
+});
+
+
+describe('makeRoutesFilter', function() {
+
+  let advisories;
+
+  beforeEach(function() {
+    advisories = workBatches[0].advisories;
+  });
+
+  it('should keep only the specified routes', function() {
+    const routesToKeep = ['4', '5', '7', 'S'];
+    const filter = makeRoutesFilter(routesToKeep);
+    const result = filter(advisories);
+
+    // result should have the specified routes and nothing more
+    result.should.be.an('object').that.has.all.keys(routesToKeep);
+    Object.keys(result).should.have.lengthOf(routesToKeep.length);
+  });
+
+  // TODO
+  // it('should ignore non-subway routes', function() {
+  // });
+
+  // TODO
+  // it('should throw an error with no argument', function() {
+  // });
+
+});
