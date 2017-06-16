e142561b5fff6e053ae7d95e21e128a181d60785
geoffreyyip <geoffreyyip2@gmail.com>
2017-06-16 00:35:16 -0400

    Connect advisory filters to workBatch structure
    Transformation methods defined in filters.js are meant to accept a
    single advisories object, not an array of workBatches. This commit adds
    a cleanser.js file to wrap those transformation methods and invoke them
    on an array of workBatches.

---
+++ subwayDelays/cleanser.js
@@ -0,0 +1,42 @@
+const {
+  makeRoutesFilter,
+  removeDuplicates,
+} = require('./filters.js');
+
+/**
+ * returns an exact copy of the passed in object but with one
+ * property changed
+ */
+const applyFilter = function(filter, object, prop) {
+  return Object.assign(
+    {},
+    object,
+    { [prop]: filter(object[prop]) }
+  );
+}
+
+// apply multiple filters to a single workBatch
+const applyAdvisoryFilters = function(workBatch, filters) {
+  if (!Array.isArray(filters)) filter = [filter];
+  // filters should be pure functions accepting and returning one object
+  return filters.reduce((result, filter) => {
+    return applyFilter(filter, result, 'advisories');
+  }, workBatch);
+}
+
+exports.cleanSubwayData = function(workBatches) {
+  return workBatches.map((workBatch) => {
+    return applyAdvisoryFilters(workBatch, [
+      removeDuplicates,
+    ]);
+  });
+}
+
+exports.filterByRoutes = function(workBatches, routesToKeep) {
+  const routesFilter = makeRoutesFilter(routesToKeep);
+  return workBatches.map((workBatch) => {
+    return applyAdvisoryFilters(workBatch, [
+      routesFilter,
+    ]);
+  });
+}
---
+++ subwayDelays/cleanser.test.js
@@ -0,0 +1,49 @@
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+
+chai.use(chaiAsPromised);
+chai.should();
+
+const {
+  cleanSubwayData,
+  filterByRoutes,
+} = require('./cleanser.js');
+
+const workBatches = require('./example.json');
+
+describe('cleanSubwayData', function() {
+
+  // TODO
+  // it('should delete blank text nodes', function () {
+  // });
+
+  it('should prune repeats in the advisories property', function() {
+    const cleaned = cleanSubwayData(workBatches);
+    cleaned.forEach((workBatch) => {
+      const advisories = workBatch.advisories;
+      const messages = [].concat(Object.keys(advisories));
+
+      messages.should.have.lengthOf(new Set(messages).size);
+    });
+  });
+
+});
+
+describe('filterByRoutes', function() {
+
+  it('should keep the same advisories for specified routes', function() {
+    const routesToKeep = ['A', 'F', 'M', 'E', 'G'];
+    const filtered = filterByRoutes(workBatches, routesToKeep);
+
+    filtered.forEach((workBatch, i) => {
+      const newAdvisories = workBatch.advisories;
+      const oldWorkBatch = workBatches[i];
+      const oldAdvisories = routesToKeep.reduce((result, route) => {
+        result[route] = oldWorkBatch.advisories[route];
+        return result;
+      }, {});
+      newAdvisories.should.deep.equal(oldAdvisories);
+    });
+  });
+
+});
