f9b16ef8bbc693e20dd56ca41f36b0326cd6527d
geoffreyyip <geoffreyyip2@gmail.com>
2017-06-19 22:44:16 -0400

    Refactor /dashboard route

--- server.js
+++ server.js
@@ -2,14 +2,6 @@ const express = require('express');
 const compression = require('compression');
 const registerNunjucks = require('./registerNunjucks');
 
-const nextBusArrivals = require('./busArrivals/nextBusArrivals');
-const plannedWork = require('./getPlannedWork/query');
-
-const {
-  cleanSubwayData,
-  filterByRoutes,
-} = require('./subwayDelays/cleanser.js');
-
 const app = express();
 
 // templating engine
@@ -18,32 +10,13 @@ registerNunjucks(app);
 // gzip compression
 app.use(compression());
 
-app.set('port', process.env.PORT || 4444);
-
-// routes
-app.get('/dashboard', async (req, res) => {
-  const {
-    busRoute,
-    busStop,
-    subwayRoutes,
-  } = req.query;
-
-  try {
-    const [arrivals, subwayDelays] = await Promise.all([
-      nextBusArrivals(busRoute, busStop),
-      filterByRoutes(cleanSubwayData(await plannedWork), subwayRoutes),
-    ]);
-
-    res.render('dashboard.html', { arrivals, subwayDelays });
-  } catch(err) {
-    console.log(err);
-    res.status(500).send('500 (INTERNAL SERVER ERROR) Something broke on our end!');
-  }
-});
-
+// routing
 app.use(require('./dashboard/settings'));
+app.use(require('./dashboard/display'));
 app.use(express.static('public'));
 
+// start listening
+app.set('port', process.env.PORT || 4444);
 app.listen(app.get('port'), () => {
   console.log(`listening on port ${app.get('port')}`)
 })
